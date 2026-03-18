212cf1b7c69490b1bea4f636310c65601906921c
Liefran Satrio Sim <Liefran123@gmail.com>
2026-03-19 00:21:03 +0700

    Feat: Header Discover View

--- coffio/ContentView.swift
+++ coffio/ContentView.swift
@@ -12,33 +12,26 @@ struct ContentView: View {
     
     var body: some View {
         TabView(selection: $selectedTab) {
-            Text("Home")
-                .tabItem {
-                    Image(selectedTab == 0 ? "ic_hot_coffee_orange" : "ic_hot_coffee_black")
-                    Text("Home")
-                }
-                .tag(0)
-
             Text("Discover")
                 .tabItem {
-                    Image(selectedTab == 1 ? "ic_pin_orange" : "ic_pin_black")
+                    Image(selectedTab == 0 ? "ic_pin_orange" : "ic_pin_black")
                     Text("Discover")
                 }
-                .tag(1)
+                .tag(0)
             
             Text("Spending")
                 .tabItem {
-                    Image(selectedTab == 2 ? "ic_bill_orange" : "ic_bill_black")
+                    Image(selectedTab == 1 ? "ic_bill_orange" : "ic_bill_black")
                     Text("Spending")
                 }
-                .tag(2)
+                .tag(1)
             
             Text("Events")
                 .tabItem {
-                    Image(selectedTab == 3 ? "ic_calendar_orange" : "ic_calendar_black")
+                    Image(selectedTab == 2 ? "ic_calendar_orange" : "ic_calendar_black")
                     Text("Spending")
                 }
-                .tag(3)
+                .tag(2)
         }
         .tint(.orange)
     }
---
+++ coffio/DiscoverView.swift
@@ -0,0 +1,95 @@
+//
+//  HomeView.swift
+//  coffio
+//
+//  Created by Liefran Satrio Sim on 18/03/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State var searchInput: String = ""
+    @State var selectedCategory: Int = 0
+    
+    var body: some View {
+        VStack(spacing: 0.0) {
+            searchBar
+            .padding(.horizontal, 24.0)
+            
+            categoriesTab
+            .padding(.horizontal, 24.0)
+            
+            Rectangle()
+                .fill(.gray)
+                .frame(width: .infinity, height: 1.0)
+                .opacity(0.25)
+            Spacer()
+        }
+    }
+    
+    var searchBar: some View {
+        HStack {
+            Spacer()
+            TextField(text: $searchInput) {
+                HStack(alignment: .bottom, spacing: 4.0) {
+                    Label("Start your Search", systemImage: "magnifyingglass")
+                }
+            }
+            Spacer()
+        }
+        .padding(.horizontal, 12.0)
+        .padding(.vertical, 8.0)
+        .overlay {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(.clear)
+                .stroke(.black, lineWidth: 0.5)
+        }
+    }
+    
+    var categoriesTab: some View {
+        HStack(alignment: .top) {
+            Spacer()
+            createCategoryMenu(label: "Cafe", imageName: "house", isSelected: selectedCategory == 0) {
+                selectedCategory = 0
+            }
+            Spacer()
+            Spacer()
+            createCategoryMenu(label: "Event", imageName: "calendar", isSelected: selectedCategory == 1) {
+                selectedCategory = 1
+            }
+            Spacer()
+        }
+        .padding(.top, 16.0)
+    }
+    
+    func createCategoryMenu(
+        label: String,
+        imageName: String,
+        isSelected: Bool,
+        onClick: @escaping () -> Void
+    ) -> some View {
+        VStack(alignment: .center, spacing: 4.0) {
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 36.0, height: 36.0)
+                .foregroundStyle(isSelected ? .orange : .black)
+            Text(label)
+                .font(.callout)
+                .foregroundStyle(isSelected ? .orange : .black)
+            
+            if isSelected {
+                RoundedRectangle(cornerRadius: 12.0)
+                    .fill(.orange)
+                    .frame(width: 16.0, height: 2.0)
+            }
+        }
+        .onTapGesture {
+            onClick()
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
