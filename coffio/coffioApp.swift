ed5e3f14eeea53ac5fad5b170bc810933e43735e
Liefran Satrio Sim <Liefran123@gmail.com>
2026-03-18 16:46:44 +0700

    Initial Commit

---
+++ coffio/coffioApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffioApp.swift
+//  coffio
+//
+//  Created by Liefran Satrio Sim on 18/03/26.
+//
+
+import SwiftUI
+
+@main
+struct coffioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
