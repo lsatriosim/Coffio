b4c1f5f382199f6e602d028c7a9e353ed115847e
Liefran Satrio Sim <Liefran123@gmail.com>
2026-03-18 23:12:05 +0700

    feat: bottom tab bar

--- coffio/ContentView.swift
+++ coffio/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $selectedTab) {
+            Text("Home")
+                .tabItem {
+                    Image(selectedTab == 0 ? "ic_hot_coffee_orange" : "ic_hot_coffee_black")
+                    Text("Home")
+                }
+                .tag(0)
+
+            Text("Discover")
+                .tabItem {
+                    Image(selectedTab == 1 ? "ic_pin_orange" : "ic_pin_black")
+                    Text("Discover")
+                }
+                .tag(1)
+            
+            Text("Spending")
+                .tabItem {
+                    Image(selectedTab == 2 ? "ic_bill_orange" : "ic_bill_black")
+                    Text("Spending")
+                }
+                .tag(2)
+            
+            Text("Events")
+                .tabItem {
+                    Image(selectedTab == 3 ? "ic_calendar_orange" : "ic_calendar_black")
+                    Text("Spending")
+                }
+                .tag(3)
         }
-        .padding()
+        .tint(.orange)
     }
 }
 
